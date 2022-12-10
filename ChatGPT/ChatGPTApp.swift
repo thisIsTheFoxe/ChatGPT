e1e8796cd00f3bbeee25b6126aa7acc35052bdff
Henrik Storch <thisisthefoxe@gmail.com>
2022-12-10 15:03:25 +1300

    Initial Commit

---
+++ ChatGPT/ChatGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatGPTApp.swift
+//  ChatGPT
+//
+//  Created by Henrik Storch on 10.12.22.
+//
+
+import SwiftUI
+
+@main
+struct ChatGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
